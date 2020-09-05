3d94b7d3536741ee619fbc36202d3c194009e2e3
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-05 21:17:58 +0530

    SahiMap TextInput Added

--- component/MainComponent.js
+++ component/MainComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ScrollView} from 'react-native';
 import SahiMap from './SahiMapComponent';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -7,15 +8,17 @@ const Stack = createStackNavigator();
 class Main extends React.Component {
     render() {
         return(
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen 
-                        name="SahiMap" 
-                        component={SahiMap}/>
-                </Stack.Navigator>
-            </NavigationContainer>
+                <NavigationContainer>
+                    <Stack.Navigator > 
+                        <Stack.Screen 
+                            name="SahiMap" 
+                            component={SahiMap}
+                            />
+                    </Stack.Navigator>
+                </NavigationContainer>
         );
     }
 }
+
 
 export  default Main;
--- component/SahiMapComponent.js
+++ component/SahiMapComponent.js
@@ -1,15 +1,49 @@
 import React from 'react';
-import {  ScrollView, Text} from 'react-native';
+import {  Text, ScrollView, TextInput,View,Button,StyleSheet} from 'react-native';
 
 
 class SahiMap extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            place:'',
+        }
+    }
     render() {
         return(
             <ScrollView style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <Text>Hello World</Text>
+                <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}>Search Places And Mark Them on Map</Text>
+
+                <View style={styles.input}>
+                    <TextInput placeholder="Enter a Place to Mark on Map" style={{ height: 40, width: 200}} value={this.state.place} onChange={this._onChangeHandler}></TextInput>
+                </View>
+                <View style={styles.buttons}>
+                    <Button title="Search" buttonStyle={{ backgroundColor: '#f4511e'}}></Button> 
+                </View>
             </ScrollView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    input: {
+        marginLeft: 30,
+        marginTop: 30,
+        marginRight: 30,
+        marginBottom: 10,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderEndWidth: 1,
+        borderLeftWidth: 1,
+        borderRadius: 10,
+        justifyContent: 'center',
+        textAlign: 'center',
+        borderColor: '#f4511e'
+    },
+    buttons: {
+        flexDirection: 'row',
+        marginLeft: 130,
+    }
+});
 
 export default SahiMap;
