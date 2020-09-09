324c99fb7fb35cd61d5325aefcc39c039df18bf4
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-09 22:13:50 +0530

    Adding Geolocation

--- component/SahiMapComponent.js
+++ component/SahiMapComponent.js
@@ -1,34 +1,48 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { ScrollView,  Text, View,StyleSheet, Dimensions} from 'react-native';
-import { Button, Icon} from 'react-native-elements';
 import MapView, { Marker } from 'react-native-maps';
-import GooglePlacesInput from './GoogleAutoComplete';
 
 class SahiMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            place:'',
+            latitude: 0,
+            longitude: 0
         }
+    }
+
+    componentDidMount() {
+        this.watchId = navigator.geolocation.watchPosition((pos) => {
+            this.setState({
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude
+            });
+        },(error) => {
+            this.setState({
+                error: error.message
+            });
+        },
+        {enableHighAccuracy: false,timeout: 1,maximumAge: 1,distanceFilter: 1}
+        )
     }
 
     render() {
         
         return(
             <React.Fragment>
-                <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}>Search Places And Mark Them on Map</Text>
-                <GooglePlacesInput />
+                <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}>Get Your Current Location And Mark Them on Map</Text>
+        <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}> Your Location is {this.state.latitude}, {this.state.longitude}</Text>
                 <ScrollView style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
                     <View style={styles.container}>
                         <MapView style={styles.mapview}
                             initialRegion={{
-                                latitude: 22.5726,
-                                longitude: 88.3639,
+                                latitude: this.state.latitude,
+                                longitude: this.state.longitude,
                                 latitudeDelta: 0.05,
                                 longitudeDelta: 0.05
                             }}
                             >
-                                <Marker coordinate={{latitude: 22.5726, longitude:88.3639}}></Marker>
+                                <Marker coordinate={{latitude: this.state.latitude, longitude:this.state.longitude}}></Marker>
                             </MapView>
                     </View>
                 </ScrollView>
