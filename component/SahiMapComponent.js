77422dfc10b8990cb012eddf508968cfa21a7dd8
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-10 00:04:36 +0530

    Adding Geolocation Updated And Removing Error

--- component/SahiMapComponent.js
+++ component/SahiMapComponent.js
@@ -1,45 +1,72 @@
 import React from 'react';
 import { ScrollView,  Text, View,StyleSheet, Dimensions} from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 class SahiMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            latitude: 0,
-            longitude: 0
+            latitude: null,
+            longitude: null,
+            location:null,
+            errorMess: null
         }
     }
-
+    
     componentDidMount() {
-        this.watchId = navigator.geolocation.watchPosition((pos) => {
+        this.functionLocationAsync();
+    }
+
+    functionLocation = () => {
+        let watchId = navigator.geolocation.watchPosition((pos) => {
             this.setState({
                 latitude: pos.coords.latitude,
                 longitude: pos.coords.longitude
             });
         },(error) => {
-            this.setState({
-                error: error.message
-            });
+
+            alert(error.message);
         },
-        {enableHighAccuracy: false,timeout: 1,maximumAge: 1,distanceFilter: 1}
+        {enableHighAccuracy: true,timeout: 20000,maximumAge: 1000}
         )
     }
 
+    functionLocationAsync =async() => {
+        let {status} = await Permissions.askAsync(Permissions.LOCATION);
+        if( status !== 'granted') {
+            this.setState({
+                errorMess: 'Permission not granted for location'
+            });
+        }
+        else{
+            this.functionLocation();
+        }
+        let location = await Location.getCurrentPositionAsync();
+        this.setState({location});
+    };
+
     render() {
-        
+        let text='';
+        if(this.state.errorMess){
+            text=this.state.errorMess;
+        }
+        else if(this.state.location) {
+            text=this.state.location;
+        }
         return(
             <React.Fragment>
                 <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}>Get Your Current Location And Mark Them on Map</Text>
-        <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}> Your Location is {this.state.latitude}, {this.state.longitude}</Text>
+                <Text style={{ textAlign: 'center', margin: 10, color: '#514DA8', fontWeight: 'bold' }}> Your Location is {this.state.latitude}, {this.state.longitude}</Text>
                 <ScrollView style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
                     <View style={styles.container}>
                         <MapView style={styles.mapview}
                             initialRegion={{
                                 latitude: this.state.latitude,
                                 longitude: this.state.longitude,
-                                latitudeDelta: 0.05,
-                                longitudeDelta: 0.05
+                                latitudeDelta: 0.0043,
+                                longitudeDelta: 0.034
                             }}
                             >
                                 <Marker coordinate={{latitude: this.state.latitude, longitude:this.state.longitude}}></Marker>
